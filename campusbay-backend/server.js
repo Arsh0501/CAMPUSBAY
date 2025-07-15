a3ad3c1d168c304b16c098f80b9587418ed07f50
arushi_anand <arushi27sep@gmail.com>
2025-07-15 23:20:53 +0530

    Fix uploads route to use persistent disk path

--- campusbay-backend/server.js
+++ campusbay-backend/server.js
@@ -17,6 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
+app.use("/uploads", express.static("/uploads"));
 
 // ✅ Extract userId from JWT (for controllers to use)
 app.use((req, res, next) => {
@@ -84,9 +85,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/listings", listingRoutes);
 app.use("/api/offers", offerRoutes);
 app.use("/api/messages", messageRoutes);
-
-// ✅ Serve uploaded images
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // ✅ Default route to serve the login page
 app.get("/", (req, res) => {
