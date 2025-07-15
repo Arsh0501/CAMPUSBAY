30f29a799c7967c2b01adc59492ef5d4b2c98444
arushi_anand <arushi27sep@gmail.com>
2025-07-15 20:45:59 +0530

    Fixed

--- campusbay-backend/models/user.js
+++ campusbay-backend/models/user.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true }, // ✅ make sure this is present
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  name: String,
+  email: { type: String, unique: true },
+  college: String,
+  password: String
 });
 
 module.exports = mongoose.model("User", userSchema);
